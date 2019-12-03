793e9b0618c20d9110875bf4d7512e24b3b47259
averagetechsupportanand <58480617+averagetechsupportanand@users.noreply.github.com>
2019-12-03 10:29:36 -0600

    Update GCI_helloWorld.java
    Hello world but will print Hello World! twice.

--- GCI_helloWorld.java
+++ GCI_helloWorld.java
@@ -8,6 +8,7 @@ public class GCI_helloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World! x2");
     }
     
 }
