21a9add8359bed1bb9848a2a59c437e62332ea6f
averagetechsupportanand <58480617+averagetechsupportanand@users.noreply.github.com>
2019-12-03 10:26:47 -0600

    Add files via upload

---
+++ GCI_helloWorld.java
@@ -0,0 +1,13 @@
+package gci_helloworld;
+
+/**
+ *
+ * @author averagetechsupportanand
+ */
+public class GCI_helloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
